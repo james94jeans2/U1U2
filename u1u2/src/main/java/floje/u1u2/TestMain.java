1602ad14e572959a598db177752684fc54393bad
Jens <Jens@FPT>
2014-04-27 18:49:13 +0200

    Test Edit

--- u1u2/src/main/java/floje/u1u2/TestMain.java
+++ u1u2/src/main/java/floje/u1u2/TestMain.java
@@ -4,23 +4,23 @@ import fpt.com.ProductList;
 
 public class TestMain {
 
-//	public static void main(String[] args) {
-//		ModelShop model = new ModelShop();
-//		ControllerShop controller = new ControllerShop();
-//		ViewShop view = new ViewShop();
-//		controller.link(model, view);
-//	}
-	
-	public static void main (String[] args) {
-		ProductList products = new floje.u1u2.ProductList();
-		for (int i = 0; i < 10; ++i) {
-			fpt.com.Product product = new Product();
-			product.setName("" + i);
-			product.setPrice(i * 3.99);
-			product.setQuantity(i * 5);
-			products.add(product);
-		}
-		ViewCostumer kunde = new ViewCostumer(((floje.u1u2.ProductList)products).toArray(new Product[0]));
+	public static void main(String[] args) {
+		ModelShop model = new ModelShop();
+		ControllerShop controller = new ControllerShop();
+		ViewShop view = new ViewShop();
+		controller.link(model, view);
 	}
+	
+//	public static void main (String[] args) {
+//		ProductList products = new floje.u1u2.ProductList();
+//		for (int i = 0; i < 10; ++i) {
+//			fpt.com.Product product = new Product();
+//			product.setName("" + i);
+//			product.setPrice(i * 3.99);
+//			product.setQuantity(i * 5);
+//			products.add(product);
+//		}
+//		ViewCostumer kunde = new ViewCostumer(((floje.u1u2.ProductList)products).toArray(new Product[0]));
+//	}
 	
 }
