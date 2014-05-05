9a38b6679b435414a68707756e9684ab0ca2bc95
Fechnas <fechner_florian@web.de>
2014-05-05 14:15:39 +0200

    Create BinaryStrategy.java

---
+++ u1u2/src/main/java/floje/u1u2/BinaryStrategy.java
@@ -0,0 +1,83 @@
+package floje.u1u2;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import fpt.com.Product;
+
+public class BinaryStrategy implements fpt.com.SerializableStrategy {
+	
+	private FileOutputStream fos;
+	private ObjectOutputStream out;
+	private FileInputStream fis;
+	private ObjectInputStream in;
+	
+	public BinaryStrategy() throws Exception {
+		
+	}
+
+	@Override
+	public Product readObject() throws IOException {
+		
+		if (fis == null) {
+			fis = new FileInputStream ("products.ser");
+		}
+		
+		if (in == null) {
+			in = new ObjectInputStream(fis);
+		}
+		
+		Product pr = null;
+		
+		try {
+				pr = (Product) (in.readObject());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return pr;
+	}
+
+	@Override
+	public void writeObject(Product obj) throws IOException {
+		
+		if (fos == null) {
+			fos = new FileOutputStream("products.ser");
+		}
+		
+		if (out == null) {
+			out = new ObjectOutputStream(fos);
+		}
+		
+		out.writeObject(obj);
+		out.flush();
+	}
+
+	@Override
+	public void close() throws IOException {
+		
+		if (out != null) {
+			out.close();
+			out = null;
+		}
+		
+		if (in != null) {
+			in.close();
+			in = null;
+		}
+		
+		if (fos != null) {
+			fos.close();
+			fos = null;
+		}
+		
+		if (fis != null) {
+			fis.close();
+		}
+		
+	}
+
+}
